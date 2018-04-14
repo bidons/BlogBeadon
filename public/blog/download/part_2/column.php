bc2ac0e8880c07668062897e465d62b6930943ca
bidon <fursin.v@gmail.com>
2018-04-14 17:47:48 +0300

    fix all

--- public/blog/download/part_2/column.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/pg.php'; //required api-base and other
-
-$objname = $_GET['objdb'];
-$query = "select paging_dbnamespace_column_prop('$objname')";
-$result = PG::dbScalar($query);
-print_r($result);
-
-?>
