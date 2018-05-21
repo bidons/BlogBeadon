e8ae41f40f11ac203ed37c21d16eecb39dc4d864
bidon <fursin.v@gmail.com>
2018-05-21 07:54:20 +0300

    fix data

--- app/config/services.php
+++ app/config/services.php
@@ -83,12 +83,10 @@ $di->set(
     'db',
     function () use ($config, $di){
         $em = $di->getShared('eventsManager');
-
-
-        if (true) {
+        
+        if ($GLOBALS['DB_LOG']) {
             $logger = new \Phalcon\Logger\Adapter\File($config->application->logDir . 'db_log.log');
-
-
+            
             $em->attach(
                 "db:beforeQuery",
                 function ($event, $connection) use ($logger) {
