976b33e0909114718d4acc109a6bfb4535f01c1b
bidon <fursin.v@gmail.com>
2018-06-06 11:30:24 +0300

    add postgres10 docker

--- backup.sh
+++ backup.sh
@@ -7,9 +7,4 @@ set -e
 
 echo -n 'Create database dump ... '
 pg_dump -h localhost -U root -p 5435  -Fc \
-    --exclude-table-data=report_* \
-    --exclude-table-data=mv_rep* \
-    --exclude-table-data=temp_* \
-    --exclude-table-data=scoring_bki_photo \
-    --exclude-table-data=scoring_bki_photo_inherits \
     blog > db.dump
