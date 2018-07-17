d4c426b444acda301f5972e062c4ecc06da5824d
bidon <fursin.v@gmail.com>
2018-07-17 11:33:45 +0300

    fix data

--- helpers.php
+++ helpers.php
@@ -23,7 +23,7 @@ function projectTree($node)
               array("href"=>"/objectdb/part6","name"=> "Пагинаторы: Пример конструктора","sname"=>"Пример конструктора")),
         'olap' =>   array(array("href"=>"/olap/index","name"=>"OLAP: Введение","sname"=>"Введение"),
                           array("href"=>"/olap/part1","name"=>"OLAP: Создание (в разработке)","sname"=>"Создание (в разработке)")),
-        'parall' => array(array("href"=>"/parall/index","name"=>"Параллельные кординаты","sname"=>"Введение")));
+        'parall' => array(array("href"=>"/parall/index","name"=>"Параллельные координаты","sname"=>"Введение")));
                           /*array("href"=>"/parall/part1","name"=>"Параллельные кординаты: Создание (в разработке)","sname"=>"Создание (в разработке)")));*/
     if ($node){
         return $data[$node];
